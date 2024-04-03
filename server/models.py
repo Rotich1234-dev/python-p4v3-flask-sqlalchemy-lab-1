6feae1e3f9ef5a165ef841d5a572862e88973fe0
Rotich <jane.rotich@student.moringaschool.com>
2024-04-03 16:23:52 +0300

    feat:complete assignment

--- server/models.py
+++ server/models.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
+from sqlalchemy import MetaData, Column, Integer, Float, String
 from sqlalchemy_serializer import SerializerMixin
 
 metadata = MetaData()
@@ -7,3 +7,25 @@ metadata = MetaData()
 db = SQLAlchemy(metadata=metadata)
 
 # Add models here
+class Earthquake(db.Model, SerializerMixin):
+    '''Earthquake model in models.py'''
+
+    __tablename__ = 'earthquakes'
+
+    id = Column(Integer, primary_key=True)
+    magnitude = Column(Float)
+    location = Column(String)
+    year = Column(Integer)
+
+    def __init__(self, magnitude=None, location=None, year=None):
+        self.magnitude = magnitude
+        self.location = location
+        self.year = year
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'magnitude': self.magnitude,
+            'location': self.location,
+            'year': self.year
+        }
